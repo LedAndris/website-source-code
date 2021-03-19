be3b5fc092c1143168e35f14a48188d8850be055
LedAndris <ledererandras2004@gmail.com>
2021-03-19 14:52:27 +0100

    fixed error handling

--- pages/api/apply.js
+++ pages/api/apply.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 export default function handler(req, res) {
     axios.post("process.env.WEBHOOK_URL", req.body).then(resp => {
-        res.status(200).send("process.env.WEBHOOK_URL")
+        res.status(200).send(process.env.WEBHOOK_URL)
     }).catch(err => {
-        res.status(500).send("error")
+        if (err != NaN) {
+            res.status(500).send(process.env.WEBHOOK_URL)
+        }
     })
 }
